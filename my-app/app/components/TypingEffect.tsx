ba9c2fb3fd7cf9290dfc8d906757a5eae2c7f4fa
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-04-08 07:52:45 +0200

    Improve the home made typing effect with 2 props. 1. myText and className for a better flexibility

--- my-app/app/components/TypingEffect.tsx
+++ my-app/app/components/TypingEffect.tsx
@@ -1,13 +1,18 @@
 "use client";
-import React from "react";
+import React, { FC } from "react";
 import { useState, useEffect } from "react";
 
 // this is my home made component to make type effect 
 
-const TypingEffect = () => {
+type ComponentProps = {
+    myText: string;
+    className?: string;
+    classNameText?: string;
+}
+
+const TypingEffect: FC<ComponentProps> = ({ myText, className, classNameText }) => {
     const [typing, setTyping] = useState<string>("");
     const [index, setIndex] = useState<number>(0);
-    const myText: string = "🐣 Bonne vacances les Pious Pious 🐣";
     const destructuredText: string[] = myText.split("");
 
     useEffect(() => {
@@ -20,9 +25,9 @@ const TypingEffect = () => {
     });
 
     return (
-        <div className="flex">
-            <p>{typing}</p>
-            <div className="inline bg-amber-50 w-0.5 h-4 ml-1 mt-1 animate-pulse" ></div>
+        <div className={"flex " + className}>
+            <p className={classNameText}>{typing}</p>
+            <div className="inline bg-amber-50 w-1 h-7.5 ml-1 mt-1 animate-pulse" ></div>
         </div>
     );
 };
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,10 @@
+import TypingEffect from "./components/TypingEffect";
 import WelcomeMessage from "./components/WelcomeMessage";
 
 export default function Home() {
   return (
     <div className="h-dvh flex items-center justify-center">
-      <WelcomeMessage />
+      <TypingEffect myText="<Mehdi-Godefroid />" className="text-3xl"/>
     </div>
   );
 }
